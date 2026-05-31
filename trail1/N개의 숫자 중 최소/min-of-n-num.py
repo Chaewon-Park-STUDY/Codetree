8fa3bcb91052691e0a08d6028e937160d157e444
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-05-31 16:32:08 +0900

    [Passed] N개의 숫자 중 최소 (53ms, 16MB)

---
+++ trail1/N개의 숫자 중 최소/min-of-n-num.py
@@ -0,0 +1,22 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+min=10
+num=0
+
+for i in range(1,n):
+    if a[i-1] >= a[i]:
+        min= a[i]
+
+for elem in a:
+    if min<= elem:
+        pass
+    else:
+        min= elem
+
+for cnt in a:
+    if cnt==min:
+        num+=1
+
+print(min, num, end= " ")
