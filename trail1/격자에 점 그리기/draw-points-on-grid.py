5397db41f2a078a54c33df5f4f408a2e3ac60d01
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-04 16:12:00 +0900

    [Passed] 격자에 점 그리기 (53ms, 16MB)

---
+++ trail1/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,15 @@
+N, M= map(int, input().split())
+
+arr= [
+    [0 for _ in range(N)]
+    for _ in range(N)
+]
+
+for i in range(M):
+    r,c= tuple(map(int, input().split()))
+    arr[r-1][c-1]= i+1
+
+for row in arr:
+    for elem in row:
+        print(elem, end= " ")
+    print()
