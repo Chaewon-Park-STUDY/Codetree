1356a6a55628963db10c01a934178c85719db5ff
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-04 15:54:30 +0900

    [Passed] 동전이 있는 위치 (56ms, 16MB)

---
+++ trail1/동전이 있는 위치/Where-coins-are-located.py
@@ -0,0 +1,19 @@
+N, M= map(int, input().split())
+
+
+arr=[
+    [0 for _ in range(N)]
+    for _ in range(N)
+]
+
+
+for i in range(M):
+    r,c= tuple(map(int, input().split()))
+    arr[r-1][c-1]=1
+
+
+for row in arr:
+    for elem in row:
+        print(elem, end= " ")
+    print()
+
