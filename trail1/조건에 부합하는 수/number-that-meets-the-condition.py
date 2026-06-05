dce004f9b473afafdfdde22f76562024ffd5dfba
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-05 16:03:04 +0900

    [Passed] 조건에 부합하는 수 (53ms, 16MB)

---
+++ trail1/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -0,0 +1,15 @@
+A= int(input())
+arr=[]
+
+for i in range(1, A+1):
+    if i%2==0 and i%4!=0:
+        pass
+    elif (i//8)%2==0:
+        pass
+    elif i%7<4:
+        pass
+    else:
+        arr.append(i)
+
+for elem in arr:
+    print(elem, end= " ")
