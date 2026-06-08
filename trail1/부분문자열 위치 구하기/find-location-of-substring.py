c10c5098d15961042f10927492f815706adf27fe
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-08 16:30:48 +0900

    [Passed] 부분문자열 위치 구하기 (49ms, 16MB)

---
+++ trail1/부분문자열 위치 구하기/find-location-of-substring.py
@@ -0,0 +1,15 @@
+input_str = input()
+target_str = input()
+
+# Please write your code here.
+
+for i in range(len(input_str)-(len(target_str)-1)):
+    if all(input_str[i+j]== target_str[j] for j in range(len(target_str))):
+        print(i)
+        break
+
+if target_str not in input_str:
+    print(-1)
+# for i in range(len(input_str)-(len(target_str)-1)):
+#     if not all(input_str[i+j]== target_str[j] for j in range(len(target_str))):
+#         print(-1)
