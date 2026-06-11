c2facde2f99daaa12543f952e9fe115c73c01825
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-12 07:26:07 +0900

    [Passed] 건너뛰고 별표 출력하기 (48ms, 16MB)

---
+++ trail1/건너뛰고 별표 출력하기/skip-and-print-a-star.py
@@ -0,0 +1,14 @@
+N= int(input())
+
+
+for i in range(N):
+    for j in range(i+1):
+        print("*", end="")
+    print()
+    print()
+
+for i in range(N-1):
+    for j in range(N-1-i):
+        print("*", end= "")
+    print()
+    print()
