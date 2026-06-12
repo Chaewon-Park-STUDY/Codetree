954283a7444147915d57d93c4d556db4a8d69088
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-13 07:50:16 +0900

    [Passed] 체크 다이아몬드 (54ms, 16MB)

---
+++ trail1/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,16 @@
+N= int(input())
+
+
+for i in range(N):
+    for j in range(N-1-i):
+        print(" ", end="")
+    for k in range(i+1):
+        print("*", end= " ")
+    print()
+
+for i in range(N-1):
+    for j in range(i+1):
+        print(" ", end="")
+    for k in range(N-1-i):
+        print("*", end=" ")
+    print()
