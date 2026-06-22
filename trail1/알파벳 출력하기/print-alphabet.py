7acb76b89a39e742290d9bc199532e3e0529c0d1
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-22 16:39:59 +0900

    [Passed] 알파벳 출력하기 (47ms, 16MB)

---
+++ trail1/알파벳 출력하기/print-alphabet.py
@@ -0,0 +1,10 @@
+N= int(input())
+
+cnt=65
+for i in range(N):
+    for j in range(i+1):
+        print(chr(cnt),end="")
+        cnt+=1
+        if ord(chr(cnt))>ord("Z"):
+            cnt=65
+    print()
