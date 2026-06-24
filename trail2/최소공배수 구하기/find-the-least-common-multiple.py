fcb8f4197291fe3951431e7f3317c5f95d4aa04e
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-24 12:03:21 +0900

    [Passed] 최소공배수 구하기 (56ms, 16MB)

---
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+
+
+def logic(n,m):
+    num=0
+    for i in range(1,min(n,m)+1):
+        if n%i==0 and m%i==0:
+            num=i
+    print(num*(n//num) * (m//num))
+logic(n,m)
