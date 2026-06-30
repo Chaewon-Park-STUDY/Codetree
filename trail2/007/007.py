d054cbe62041c63d32b0b68f4e83551b00cd5af3
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-06-30 22:59:44 +0900

    [Passed] 007 (48ms, 16MB)

---
+++ trail2/007/007.py
@@ -0,0 +1,17 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+
+
+
+class Secret():
+    def __init__(self, secret_code, meeting_point, time):
+        self.s= f"secret code : {secret_code}"
+        self.m= f"meeting point : {meeting_point}"
+        self.t= f"time : {time}"
+    
+new= Secret(secret_code, meeting_point, time)
+print(new.s)
+print(new.m)
+print(new.t)
