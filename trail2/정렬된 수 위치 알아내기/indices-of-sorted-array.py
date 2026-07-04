e7012a1961c81e92dc3bd6a0451c414319c15f49
CHAEWON_PARK <chelsea913@yonsei.ac.kr>
2026-07-04 16:37:36 +0900

    [Passed] 정렬된 수 위치 알아내기 (55ms, 16MB)

---
+++ trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,15 @@
+n = int(input())
+sequence = list(map(int, input().split()))
+
+# Please write your code here.
+new=[]
+for idx, elem in enumerate(sequence,start=1):
+    new.append((idx, elem))
+
+
+new.sort(key=lambda x: x[1])
+
+for i in range(1,n+1):
+    for _ in range(n):
+        if new[_][0]==i:
+            print(_+1, end=" ")
