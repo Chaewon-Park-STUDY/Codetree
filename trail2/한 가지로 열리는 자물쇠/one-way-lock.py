79a3eb3ef541ffebfe601e3352cf432e988ccd7e
Chelsea <chelsea913@yonsei.ac.kr>
2026-07-09 13:04:11 +0900

    [Passed] 한 가지로 열리는 자물쇠 (60ms, 17MB)

---
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,14 @@
+N = int(input())
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+new=[]
+cnt=0
+for i in range(1,N+1):
+        if abs(i-a)>2:
+            for j in range(1,N+1):
+                if abs(j-b)>2:
+                    for k in range(1,N+1):
+                        if abs(k-c)>2:
+                            cnt+=1
+print(N**3-cnt)
