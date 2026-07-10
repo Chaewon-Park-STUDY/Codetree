6afae57f9f40d47c24665113a79cb1ed4627cc98
Chelsea <chelsea913@yonsei.ac.kr>
2026-07-11 07:47:35 +0900

    [Passed] 정수의 조건 여부 3 (60ms, 16MB)

---
+++ trail1/정수의 조건 여부 3/numbers-condition-3.py
@@ -0,0 +1,16 @@
+a= int(input())
+
+if a%13==0 or a%19==0:
+    print("True")
+else:
+    print("False")
+
+
+# #sol2)
+# def figure(a):
+#     if a%13==0 or a%19==0:
+#         return True
+#     else:
+#         return False
+# new=figure(a)
+# print(new)
