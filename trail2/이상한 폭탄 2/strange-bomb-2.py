9ad80847cfffc7c0777181bc32756bb22eb06378
Chelsea <chelsea913@yonsei.ac.kr>
2026-07-22 15:14:03 +0900

    [Passed] 이상한 폭탄 2 (58ms, 16MB)

---
+++ trail2/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,24 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+
+store=[]
+
+
+def bomb(a,b,k):
+    if b-a<=k:
+        return True
+
+for i in range(N-1):
+    for j in range(i+1,N):
+        if num[i]==num[j] and bomb(i,j,K):
+            store.append(num[i])
+
+if len(store)>=1:            
+    print(max(store))
+else:
+    print(-1)
+
+
+
