848501976ab6a334c4af3566068f9b0b37b268f9
Chelsea <chelsea913@yonsei.ac.kr>
2026-07-25 15:24:29 +0900

    [Passed] 원 모양으로 되어있는 방 (88ms, 18MB)

---
+++ trail2/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -0,0 +1,23 @@
+n = int(input())
+a = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+
+def check(i,j):
+    if i+j>n-1:
+        return a[i+j-n]
+    else:
+        return False
+
+min_value=10**9
+for i in range(n):
+    sum_total=0
+    for j in range(1,n):
+        if check(i,j)==False:
+            sum_total+=j*a[i+j]
+        else:
+            sum_total+=j*check(i,j)
+    min_value= min(min_value,sum_total)
+print(min_value)
+    
