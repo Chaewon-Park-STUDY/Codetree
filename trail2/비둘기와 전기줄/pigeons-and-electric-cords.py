4c89a15ff7750f239fa1440f411c7e02b44c526e
chaewon <chelsea913@yonsei.ac.kr>
2026-07-29 12:52:06 +0900

    [Passed] 비둘기와 전기줄 (48ms, 16MB)

---
+++ trail2/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -0,0 +1,46 @@
+N = int(input())
+pigeon = []
+position = []
+for _ in range(N):
+    p, pos = map(int, input().split())
+    pigeon.append(p)
+    position.append(pos)
+
+# Please write your code here.
+
+store=[]
+
+for i in range(N):
+    store.append((pigeon[i],position[i]))
+
+arr=[0 for _ in range(11)] #1 index는 1번 비둘기의 원래 위치, 시작포인트
+
+
+new=[]
+for i in range(N):
+    if store[i][0] not in new:
+        new.append(store[i][0])
+        arr[store[i][0]]= store[i][1]
+
+
+
+total=0
+for i in range(len(store)):
+    for j in range(1,11):
+        if store[i][0]==j and store[i][1]!=arr[j]:
+            if store[i][1]==0:
+                arr[j]=0
+                total+=1
+            else:
+                arr[j]=1
+                total+=1
+print(total)
+
+
+
+
+# for elem in store:
+#     if elem[0] in num:
+#         num.pop(elem[0])
+#         arr[elem[0]]=elem[1]
+# print(arr)
