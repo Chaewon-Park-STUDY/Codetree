bc3935b5044b68b4c47ec1ed8c20506998a14f7a
chaewon <chelsea913@yonsei.ac.kr>
2026-08-24 15:11:05 +0900

    [Passed] 정수 사각형 최댓값의 최소 (79ms, 18MB)

---
+++ trail4/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -0,0 +1,20 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+for i in range(1,n):
+    grid[0][i]=max(grid[0][i],grid[0][i-1])
+    grid[i][0]=max(grid[i][0],grid[i-1][0])
+
+for i in range(1,n):
+    for j in range(1,n):
+        grid[i][j]=min(max(grid[i][j],grid[i-1][j]),max(grid[i][j],grid[i][j-1]))
+
+min_val=100000000
+
+for i in range(1,n):
+    for j in range(1,n):
+        min_val=min(min_val,grid[i][j])
+
+print(grid[n-1][n-1])
