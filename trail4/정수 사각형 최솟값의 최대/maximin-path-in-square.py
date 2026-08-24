b34e236c505935cf0a2498036d6751472203edee
chaewon <chelsea913@yonsei.ac.kr>
2026-08-24 12:14:55 +0900

    [Passed] 정수 사각형 최솟값의 최대 (50ms, 16MB)

---
+++ trail4/정수 사각형 최솟값의 최대/maximin-path-in-square.py
@@ -0,0 +1,14 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+
+for i in range(1,n):
+    grid[0][i]=min(grid[0][i],grid[0][i-1])
+    grid[i][0]=min(grid[i-1][0],grid[i][0])
+
+for i in range(1,n):
+    for j in range(1,n):
+        grid[i][j]=min(grid[i][j],max(grid[i-1][j],grid[i][j-1]))
+
+print(grid[n-1][n-1])
