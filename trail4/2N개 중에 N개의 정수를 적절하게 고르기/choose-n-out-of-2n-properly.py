63ae9b027bc945ac71eeec34ae14d4f2229717d8
chaewon <park.chaewon913@gmail.com>
2026-08-29 15:36:55 +0900

    [Passed] 2N개 중에 N개의 정수를 적절하게 고르기 (437ms, 29MB)

---
+++ trail4/2N개 중에 N개의 정수를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -0,0 +1,29 @@
+n = int(input())
+num = list(map(int, input().split()))
+
+# Please write your code here.
+
+total=sum(num)
+
+store=[]
+
+def sum_val(arr):
+    a=sum(arr)
+    store.append(abs(a-(total-a)))
+
+
+arr=[]
+start=0
+
+def choose(start,n):
+    if len(arr)==n:
+        return sum_val(arr)
+
+    for i in range(start,2*n):
+        arr.append(num[i])
+        start=i
+        choose(start+1,n)
+        arr.pop()
+
+choose(start,n)
+print(min(store))
